817d80475862e1b8c7249c5b348568cab073343b
Danny White <white.daniel.james@gmail.com>
2018-07-22 15:26:39 -0400

    recognise touches
    from iOS mirro

---
+++ Bike NYC/Assets/Scripts/TouchScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScript : MonoBehaviour
+{
+    void Update () {
+
+    }
+
+		void OnGUI () {
+			foreach(Touch myTouch in Input.touches) {
+				string message = "";
+				message += "ID: " + myTouch.fingerId + "\n";
+				message += "Phase: " + myTouch.phase.ToString() + "\n";
+				message += "ID: " + myTouch.tapCount + "\n";
+				message += "Pos X: " + myTouch.position.x + "\n";
+				message += "Pos Y: " + myTouch.position.y + "\n";
+
+				int num = myTouch.fingerId;
+				GUI.Label(new Rect(0 + 130 * num, 0, 120, 100), message);
+			}
+		}
+}
