3b7e95b3f5928022c6c775df197ee243bbc4b864
Danny White <white.daniel.james@gmail.com>
2018-07-22 16:45:18 -0400

    removed old touch code
    From wrong component

--- Bike NYC/Assets/Scripts/MoveScript.cs
+++ Bike NYC/Assets/Scripts/MoveScript.cs
@@ -11,27 +11,6 @@ public class MoveScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
-
-		Touch myTouch = Input.GetTouch(0);
-
-		if (myTouch.phase == TouchPhase.Began) {
-			Debug.Log(myTouch.position.x + "\n");
-
-			if (myTouch.position.x < Screen.width/2) {
-				Debug.Log("Left side of screen");
-			} else {
-				Debug.Log("Right side of screen");
-			}
-			// Debug.Log("touch phase began y'all");
-		}
-
-
-
-		// if (myTouch.position.x == 1) {
-		// 	Debug.Log("is 1");
-		// } else {
-		// 	Debug.Log("is something else");
-		// }
+		
 	}
 }
